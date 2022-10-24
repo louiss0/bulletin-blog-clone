147b7e2c0868a9d57e861d73481b4091ad1734b4
Shelton Louis <louisshelton0@gmail.com>
2022-10-23 22:00:51 -0400

    Had to change the astro config and also alter the post component

--- app/astro.config.ts
+++ app/astro.config.ts
@@ -7,7 +7,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  
+  site: "https://beamish-biscochitos-390e42.netlify.app",
   integrations: [tailwind(), vue({
     
     jsx: {
